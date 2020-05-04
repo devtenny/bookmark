30e62c6b7ac8ec2164ab0e3ec83b6c0f814ef3a5
taetae <ty980407@gmail.com>
2020-05-04 22:43:17 +0900

    200504 Bookmark Service

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = '6oqt&g39wsu_340(-zc3pv_a++^3sdwp^x6fcl!)ng4v6%)6jp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # 이 부분은 True로 설정해도 배포 가능함
 
-# ALLOWED_HOSTS = ['*']  # 이 부분은 반드시 지정해야 배포 가능함!!!
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # 이 부분은 반드시 지정해야 배포 가능함!!!
+# ALLOWED_HOSTS = []
 
 
 # Application definition
